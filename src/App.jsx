b9ed4d43148737b092189dffa7742b66d9a2d2c1
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-13 23:05:00 +0300

    feat: add styles to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter.jsx";
+import '../styles/app.css';
+import PostItem from "./components/PostItem.jsx";
 
 function App() {
     const [value, setValue] = useState('Some string');
 
     return (
-        <div>
+        <div className="App">
             <h3>{value}</h3>
             <input type="text"
                    value={value}
                    onChange={event => setValue(event.target.value)}
             />
             <Counter/>
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
         </div>
     )
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+const PostItem = () => {
+    return (
+        <div className="post">
+            <div className="post_content">
+                <strong>1. JavaScript</strong>
+                <div>
+                    JavaScript - язык программирования
+                </div>
+            </div>
+            <div className="post__buttons">
+                <button>Удалить</button>
+            </div>
+        </div>
+    );
+};
+
+export default PostItem;
