a93c0fd62cc06e22a68b77a240c436d3af84a94b
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-13 23:55:20 +0300

    feat: style of use ref, when I can set ref in tag whatever I want

--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from "./MyInput.module.css";
 
-const MyInput = (props) => {
+const MyInput = React.forwardRef((props, ref) => {
     return (
         <input
+            ref={ref}
             className={classes.myInput}
             {...props}
         />
     );
-};
+});
 
 export default MyInput;
