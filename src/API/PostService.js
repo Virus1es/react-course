fb092cba14d89710a1695b4c9d479e88616fd6a1
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-18 23:54:23 +0300

    create: create service for get posts

---
+++ src/API/PostService.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export default class PostService {
+    static async getAll() {
+        return  await axios.get('https://jsonplaceholder.typicode.com/posts');
+    }
+}
