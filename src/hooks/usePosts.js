f1de6c9c8090f914ea544ea0c88719cc374a2796
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-20 00:12:13 +0300

    feat: add new useMemo for posts pages array

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,5 +1,17 @@
 import {useMemo} from "react";
 
+export const usePagedPost = (totalPages) => {
+    return useMemo(() => {
+        let pagesArray = [];
+
+        for (let i = 0; i < totalPages; i++) {
+            pagesArray.push(i + 1);
+        }
+
+        return pagesArray;
+    }, [totalPages]);
+}
+
 export const useSortedPost = (posts, sort) => {
     return useMemo(() => {
         return sort ?
