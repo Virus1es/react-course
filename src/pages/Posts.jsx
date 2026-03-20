b1020391bf86995b0e134fd91dee8c314358ddfe
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-21 00:04:13 +0300

    create: create new pages for app

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles/App.css';
+import {BrowserRouter} from "react-router-dom";
+
+function Posts() {
+    return (
+        <BrowserRouter>
+
+        </BrowserRouter>
+    )
+}
+
+export default Posts;
