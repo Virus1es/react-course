fc4c5b86ccbe7fb0bb96048d56b7e49929697a01
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-21 00:12:35 +0300

    feat: add routes to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import './styles/App.css';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Posts from "./pages/Posts.jsx";
+import About from "./pages/About.jsx";
 
 function App() {
-
-
     return (
         <BrowserRouter>
-
+            <Routes>
+                <Route path="posts" element={<Posts/>}/>
+                <Route path="about" element={<About/>}/>
+            </Routes>
         </BrowserRouter>
     )
 }
