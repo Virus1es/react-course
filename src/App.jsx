1c036467572911b6bec3ae27987d9fc10077b9a1
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-21 00:30:44 +0300

    feat: use page error for not founded pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './styles/App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Posts from "./pages/Posts.jsx";
 import About from "./pages/About.jsx";
+import Error from "./pages/Error.jsx";
 import Navbar from "./components/UI/navbar/Navbar.jsx";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="posts" element={<Posts/>}/>
                 <Route path="about" element={<About/>}/>
+                <Route path="*" element={<Error/>}/>
             </Routes>
         </BrowserRouter>
     )
