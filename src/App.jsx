e82f19f2b3a019d5181f64e3497d304e3eb6c329
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-21 00:25:24 +0300

    feat: use navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import './styles/App.css';
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Posts from "./pages/Posts.jsx";
 import About from "./pages/About.jsx";
+import Navbar from "./components/UI/navbar/Navbar.jsx";
 
 function App() {
     return (
         <BrowserRouter>
-            <div className="navbar">
-                <div className="navbar__links">
-                    <Link to="/about">О сайте</Link>
-                    <Link to="/posts">Посты</Link>
-                </div>
-            </div>
+            <Navbar/>
             <Routes>
                 <Route path="posts" element={<Posts/>}/>
                 <Route path="about" element={<About/>}/>
