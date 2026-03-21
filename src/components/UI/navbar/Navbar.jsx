1031614b21e02f27dfd3e4ce7f1728b62cc04b0a
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-21 22:33:05 +0300

    feat: add button to logout

--- src/components/UI/navbar/Navbar.jsx
+++ src/components/UI/navbar/Navbar.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Link} from "react-router-dom";
 import cl from "./Navbar.module.css"
+import MyButton from "../button/MyButton.jsx";
+import {AuthContext} from "../../../context/index.js";
 
 const Navbar = () => {
+    const {isAuth, setIsAuth} = useContext(AuthContext);
+
+    const logout = () => {
+        setIsAuth(false);
+    }
+
     return (
         <div className={cl.navbar}>
+            {isAuth &&
+                <MyButton onClick={logout}>
+                    Выйти
+                </MyButton>
+            }
             <div className={cl.navbar__links}>
                 <Link to="/about">О сайте</Link>
                 <Link to="/posts">Посты</Link>
