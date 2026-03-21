9b93299ae0ba937ccf672e3cdb1b48976af9ce0b
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-21 23:02:49 +0300

    feat: use new custom hook

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -11,6 +11,7 @@ import Loader from "../components/UI/loader/Loader.jsx";
 import {useFetching} from "../hooks/useFetching.js";
 import {getPagesCount} from "../utils/pages.js";
 import Paginator from "../components/UI/pagination/Paginator.jsx";
+import {useObserver} from "../hooks/useObserver.js";
 
 function Posts() {
     const [posts, setPosts] = useState([]);
@@ -29,8 +30,6 @@ function Posts() {
 
     const lastElement = useRef(null);
 
-    const observer = useRef(null);
-
     const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
         const response = await PostService.getAll(limit, page);
         setPosts([...posts, ...response.data]);
@@ -43,20 +42,12 @@ function Posts() {
         setModal(false);
     }
 
-    useEffect(() => {
-        if(isPostsLoading) return;
-
-        if(observer.current) observer.current.disconnect();
-
-        const callback = function (entries, observer) {
-            if (entries[0].isIntersecting && page < totalPages) {
-                setPage(page + 1);
-            }
-        }
-
-        observer.current = new IntersectionObserver(callback);
-        observer.current.observe(lastElement.current);
-    }, [isPostsLoading])
+    useObserver(
+        lastElement,
+        page < totalPages,
+        isPostsLoading,
+        () => setPage(page + 1)
+    );
 
     useEffect(() => {
         void fetchPosts(limit, page);
