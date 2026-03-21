320b5fa205db914db9c4381de212242ea224d63a
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-21 22:18:42 +0300

    feat: separate routes to public and private

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,10 +2,15 @@ import About from "../pages/About.jsx";
 import Posts from "../pages/Posts.jsx";
 import PostIdPage from "../pages/PostIdPage.jsx";
 import Error from "../pages/Error.jsx";
+import Login from "../pages/Login.jsx";
 
-export const routes = [
-    {path: '/about',     element: About},
+export const privateRoutes = [
     {path: '/posts',     element: Posts},
     {path: '/posts/:id', element: PostIdPage},
+    {path: '/about',     element: About},
     {path: '*', element: Error},
+]
+
+export const publicRoutes = [
+    {path: '/login',     element: Login},
 ]
