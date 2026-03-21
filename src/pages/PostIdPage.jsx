af0d86e68c3c6b2634b9f193d201f5f650682f2a
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-21 21:43:47 +0300

    feat: print post id and title to page

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import {useFetching} from "../hooks/useFetching.js";
+import PostService from "../API/PostService.js";
+import Loader from "../components/UI/loader/Loader.jsx";
 
 const PostIdPage = () => {
+    const params = useParams();
+    const [post, setPost] = useState({});
+    const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+        const response = await PostService.getById(id);
+        setPost(response.data);
+    });
+
+    useEffect(() => {
+        fetchPostById(params.id);
+    }, []);
+
     return (
         <div>
-            <h1>а вот и пост</h1>
+            <h1>Вы открыли страницу поста (ID: {params.id})</h1>
+            {isLoading ?
+                <Loader/> :
+                <div>
+                    {post.id}. {post.title}
+                </div>
+            }
         </div>
     );
 };
